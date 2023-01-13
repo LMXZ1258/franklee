2963de6c051d10eea743e6e65272673d03674eb9
LMXZ1258 <111029600+LMXZ1258@users.noreply.github.com>
2023-01-13 22:34:54 +0800

    Update script_life.js

--- js/script_life.js
+++ js/script_life.js
@@ -1,6 +1,6 @@
 let images = document.querySelector('.images')
 let image = document.querySelector('.image')
-let n = 30;
+let n = 32;
 for (let i = n-1; i >= 1; i--) {
     var lis = document.createElement('div')
     lis.innerHTML = `<img class="img" src='images/life/img (` + i + `).png'>`
