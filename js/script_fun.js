d04f32ff1baabcd28973449680e3223babc5f897
LMXZ1258 <111029600+LMXZ1258@users.noreply.github.com>
2023-01-30 09:09:00 +0800

    Create script_fun.js

---
+++ js/script_fun.js
@@ -0,0 +1,22 @@
+let images = document.querySelector('.images')
+let image = document.querySelector('.image')
+let n = 2;
+for (let i = n-1; i >= 1; i--) {
+    var lis = document.createElement('div')
+    lis.innerHTML = `<img class="img" src='images/fun/img (` + i + `).png'>`
+    images.appendChild(lis)
+}
+let img = document.querySelectorAll('.img')
+for (let i = 0; i < img.length; i++) {
+    img[i].addEventListener("click", () => {
+        image.style.display = "flex"
+        let index = n - i - 1;
+        image.innerHTML = "<img src='images/fun/img (" + index + ").png'>"
+        document.body.style.overflow = 'hidden'; //禁止页面滑动
+    })
+}
+image.addEventListener('click', () => {
+    image.style.display = "none"
+    document.body.style.overflow = 'auto'; //出现滚动条
+})
+
